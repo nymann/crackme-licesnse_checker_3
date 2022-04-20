68800bd9b7f79b1ef996fe9c9c54d1421924f405
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-04-20 16:50:50 +0200

    :sparkles: Write keygen

--- src/crackme_licesnse_checker_3/main.py
+++ src/crackme_licesnse_checker_3/main.py
@@ -1,11 +1,23 @@
+from random import randint
+
 import typer
 
 app = typer.Typer()
 
 
+def random(max: int) -> str:
+    numbers = []
+    while sum(numbers) < max:
+        numbers.append(randint(0, 9))
+    total: int = sum(numbers)
+    diff = total - max
+    numbers[-1] -= diff
+    return "".join([str(n) for n in numbers])
+
+
 @app.command()
-def welcome(name: str) -> None:
-    typer.echo(f"Welcome {name}!")
+def keygen() -> None:
+    typer.echo(random(50))
 
 
 if __name__ == "__main__":
